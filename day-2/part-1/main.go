8685fc23869dbc4d45cd352a1fd9e6f037e5ac94
Brad Hoffman <brad.hoffman@openly.com>
2021-12-02 15:16:29 -0500

    changed text file name and finished part 1 solution

--- day-2/part-1/main.go
+++ day-2/part-1/main.go
@@ -1,3 +1,45 @@
 package main
 
-func main() {}
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
+var depth, dist int
+
+func main() {
+	f, err := os.Open("../input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	s := bufio.NewScanner(f)
+
+	for s.Scan() {
+		cmd := s.Text()
+		args := strings.Split(cmd, " ")
+
+		instr := args[0]
+		val, err := strconv.Atoi(args[1])
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		switch instr {
+		case "forward":
+			dist = dist + val
+		case "down":
+			depth = depth + val
+		case "up":
+			depth = depth - val
+		default:
+			log.Fatal("unrecognized instruction:", cmd)
+		}
+	}
+
+	fmt.Println("Distance * Depth:", dist*depth)
+}
